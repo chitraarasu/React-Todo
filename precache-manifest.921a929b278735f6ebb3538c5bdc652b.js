b44c932fb043565f963c10f4f89dcc3495e7d9bd
chitraarasu .k <kchitraarasu@gmail.com>
2021-11-15 18:09:36 +0530

    Updates

---
+++ precache-manifest.921a929b278735f6ebb3538c5bdc652b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff28fc7645f065cdda6b67394540a9c5",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "54bb39d2b56c06ecefca",
+    "url": "/React-Todo/static/js/2.8acd2808.chunk.js"
+  },
+  {
+    "revision": "9beba96e1583c99e4107",
+    "url": "/React-Todo/static/js/main.724c75c9.chunk.js"
+  },
+  {
+    "revision": "2724fc7cd8370a0b150b",
+    "url": "/React-Todo/static/js/runtime-main.219388ea.js"
+  }
+]);
